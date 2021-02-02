88dbac9dde7582f4c86967591bd3178d9ba2e135
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-02-02 11:07:05 -0600

    initial commit with tables

---
+++ subqueries.sql
@@ -0,0 +1,89 @@
+/* teams_germany table fields are 
+
+id
+team_api_id
+team_long_name
+team_short_name
+
+*/
+
+/* matches_germany table fields are 
+
+id
+country_id
+season
+stage
+date
+hometeam_id
+awayteam_id
+home_goal
+away_goal
+
+*/
+
+/* teams_spain table fields are 
+
+id
+team_api_id
+team_long_name
+team_short_name
+
+*/
+
+/* matches_spain table fields are 
+
+id
+country_id
+season
+stage
+date
+hometeam_id
+awayteam_id
+home_goal
+away_goal
+
+*/
+
+/* teams_italy table fields are 
+
+id
+team_api_id
+team_long_name
+team_short_name
+
+*/
+
+/* matches_italy table fields are 
+
+id
+country_id
+season
+stage
+date
+hometeam_id
+awayteam_id
+home_goal
+away_goal
+
+*/
+
+/* country table fields are 
+
+id
+name
+
+*/
+
+/* match table fields are 
+
+id
+country_id
+season
+stage
+date
+hometeam_id
+awayteam_id
+home_goal
+away_goal
+
+*/
